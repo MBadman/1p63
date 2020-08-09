387cc9dd887f26444a5e30fdc4e77d937e439eec
MBadman <69422097+MBadman@users.noreply.github.com>
2020-08-09 17:21:56 +0300

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -1,1 +1,19 @@
+class CfgPatches
+{
+	class re_rhs_afrf_scopes
+	{
+		name = "RHSAFRF Scopes Fix";
+		author = "Dusty";
+		requiredVersion = 0.1;
+		requiredAddons[] = {"rhs_c_weapons"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+class CfgWeapons
+	{
+		model="\VSMafrf\addons\rhs_weapons\acc\scopes\1p63\rhs_1p63";
+	};
+
 
